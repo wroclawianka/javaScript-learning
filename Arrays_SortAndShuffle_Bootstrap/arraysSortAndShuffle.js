e8030ddd2fb95dff454ffb51c86ec93e2a96d920
wroclawianka <dorota.zelga@gmail.com>
2017-08-12 14:07:45 +0200

    indicateArithmeticMeanOfTheProcess function added

--- Arrays_SortAndShuffle_Bootstrap/arraysSortAndShuffle.js
+++ Arrays_SortAndShuffle_Bootstrap/arraysSortAndShuffle.js
@@ -1,35 +1,52 @@
-Array.prototype.shuffle = function() {
-    var input = this;
-
-    for (var i = 0; i < input.length; i++) {
+function shuffle(array) {
+    for (var i = 0; i < array.length; i++) {
         var randomIndex = Math.floor(Math.random() * (i + 1));
-        var randomElement = input[randomIndex];
+        var randomElement = array[randomIndex];
 
-        input[randomIndex] = input[i];
-        input[i] = randomElement;
+        array[randomIndex] = array[i];
+        array[i] = randomElement;
     }
-    return input;
+    return array;
 }
 
-Array.prototype.sort = function() {
-    var input = this;
+function sort(array) {
+    for (var index = 0; index < array.length; index++) {
 
-    for (var index = 0; index < input.length; index++) {
+        for (var i = index + 1; i < array.length; i++) {
 
-        for (var i = index + 1; i < input.length; i++) {
+            var elementToCompare = array[i]
 
-            var elementToCompare = input[i]
+            if (array[index] > elementToCompare) {
 
-            if (input[index] > elementToCompare) {
-                debugger;
                 for (var x = i; x >= index + 1; x--) {
-                    input[x] = input[x - 1];
+                    array[x] = array[x - 1];
                 }
-                input[index] = elementToCompare;
+                array[index] = elementToCompare;
             }
         }
     }
-    return input;
+    return array;
+}
+
+function indicateArithmeticMeanOfTheProcess(process, n, array) {
+    var results = [];
+
+    for (i = 0; i < n; i++) {
+        var time = new Date().getTime();
+        process(array);
+        results.push(new Date().getTime() - time);
+    }
+    var sum = results.reduce((a, b) => a + b, 0);
+    var avg = sum / results.length;
+}
+
+function createRandomArray(amound) {
+    var array = [];
+    for (i = 0; i < amound; i++) {
+        var temp = Math.floor(Math.random() * 100 + 1);
+        array.push(temp);
+    }
+    return array;
 }
 
 function createArray() {
@@ -49,12 +66,12 @@ function createArray() {
 
 function sortArray() {
 
-    document.getElementById("array-sorted").innerHTML = "My sorted list: " + array.sort();
+    document.getElementById("array-sorted").innerHTML = "My sorted list: " + sort(array);
     document.getElementById("sort").disabled = true;
 }
 
 function suffleArray() {
-    document.getElementById("array-shuffled").innerHTML = "My shuffled list: " + array.shuffle();
+    document.getElementById("array-shuffled").innerHTML = "My shuffled list: " + shuffle(array);
 }
 
 var array = [];
