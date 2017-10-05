cdd7d5fed062152681443e6fd25abab8f94b47f9
wroclawianka <dorota.zelga@gmail.com>
2017-10-05 16:22:17 +0200

    jQuery used, some minor improvments

--- Arrays_SelectRandomItem_Bootstrap/selectRandomItem.js
+++ Arrays_SelectRandomItem_Bootstrap/selectRandomItem.js
@@ -1,55 +1,58 @@
 var randomIndex = function(array) {
-    var temp = Math.floor(Math.random() * array.length)
-    return temp;
-};
+    return Math.floor(Math.random() * array.length);
+}
 
-function selectRandomElement(array) {
-    return array[randomIndex(array)];
-};
+function randomElement(array) {
+    return array[randomIndex(array)]
+}
 
 function printRandomElement(array) {
-    document.getElementById("randomSeries").innerHTML = "TV series to see today is " + selectRandomElement(array) + ".";
-};
+    $('#randomSeries').text(answer(array));
+}
+
+function answer(array) {
+    var emptyListAlert = "Select any TV series!"
+    return (array.length == 0) ? emptyListAlert : `TV series to see today is ${randomElement(array)}.`;
+}
 
 function pushCheckedElementsToArray(array) {
-    var listOfSeries = $("input:checked~span")
-
-    for (elem of listOfSeries) {
-        array.push(elem.innerHTML);
-    }
-};
-
-function submit() {
-    pushCheckedElementsToArray(series);
-    printRandomElement(series);
-};
+    var listOfSeries = $('input:checked~span')
+    listOfSeries.each(function() {
+        array.push(this.innerHTML)
+    })
+}
 
 function createSectionForEachElement(array) {
     for (element of array) {
-        createSection(element);
+        createSection(element)
     }
 }
 
 function createSection(content) {
-    var seriesList = "div[name='seriesList']";
+    var seriesList = "div[name='seriesList']"
 
-    var row = $("<div class='checkbox'></div>");
-    $(seriesList).append(row);
+    var row = $("<div class='checkbox'></div>")
+    $(seriesList).append(row)
 
-    var label = $("<label></label>");
-    $(seriesList + ">div.checkbox:last-child").append(label);
+    var label = $('<label></label>')
+    $(seriesList + '>div.checkbox:last-child').append(label)
 
-    var input = $("<input type='checkbox' name='series'><br>");
-    $(seriesList + ">div.checkbox:last-child>label").append(input);
+    var input = $("<input type='checkbox' name='series'>")
+    $(seriesList + '>div.checkbox:last-child>label').append(input)
 
-    var span = $("<span></span>").text(content);
-    $(seriesList + ">div:last-child>label>input").after(span);
+    var span = $('<span></span>').text(content)
+    $(seriesList + '>div:last-child>label>input').after(span)
 }
 
-var top10series = ["Game of Thrones", "Ozark", "Rick and Morty", "Suits", "The Walking Dead",
-    "Orange Is the New Black", "Stranger Things", "Shameless", "Vikings", "The Handmaid's Tale"
-];
+var top10series = ['Game of Thrones', 'Ozark', 'Rick and Morty', 'Suits', 'The Walking Dead',
+    'Orange Is the New Black', 'Stranger Things', 'Shameless', 'Vikings', "The Handmaid's Tale"
+]
 
-var series = [];
+var series = []
 
-createSectionForEachElement(top10series);
+createSectionForEachElement(top10series)
+
+$('#submit').on('click', function() {
+    pushCheckedElementsToArray(series)
+    printRandomElement(series)
+});
